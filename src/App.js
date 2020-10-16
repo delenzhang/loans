dad475025bc5ad7f93aa09c62a8491fcdd653fbc
张代龙 <delenzhang@tencent.com>
2020-10-16 18:47:21 +0800

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,119 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { Form, InputNumber, Button, ConfigProvider } from "antd";
+import { Area } from "@ant-design/charts";
+import compute from "./utils";
+import zhCN from "antd/es/locale/zh_CN";
+import moment from "moment";
+import "moment/locale/zh-cn";
+import "antd/dist/antd.css";
+import "./App.css";
+
+moment.locale("zh-cn");
+
+const layout = {
+  labelCol: { span: 4 },
+  wrapperCol: { span: 22 },
+};
+const initialValues = {
+  totalMoney: 100,
+  mortgageInterest: 5,
+  rate: 5,
+  totalYear: 30,
+};
 
 function App() {
+  const [data, setData ] = useState(null);
+  const onFinish = (values) => {
+    console.log("Success:", values);
+    let json = compute(values);
+    console.log(json, 12121);
+    setData(json);
+  };
+
+  const onFinishFailed = (errorInfo) => {
+    console.log("Failed:", errorInfo);
+  };
+  const config = {
+    data,
+    height: 600,
+    xField: "date",
+    yField: "value",
+    seriesField: "title",
+    xAxis: { tickCount: 100 },
+    // slider: {
+    //   start: 1,
+    //   end: 9,
+    // },
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ConfigProvider locale={zhCN}>
+      <div className="App">
+        <header className="App-header">
+          <h1>房贷方式（等额本息和等额本金）与投资利率换算</h1>
+          <Form
+            {...layout}
+            name="basic"
+            initialValues={initialValues}
+            onFinish={onFinish}
+            onFinishFailed={onFinishFailed}
+          >
+            <Form.Item label="贷款金额">
+              <Form.Item
+                noStyle
+                name="totalMoney"
+                rules={[{ required: true, message: "必须输入贷款金额！" }]}
+              >
+                <InputNumber min={1} />
+              </Form.Item>
+              <span className="ant-form-text">万元</span>
+            </Form.Item>
+
+            <Form.Item label="贷款利息">
+              <Form.Item
+                name="mortgageInterest"
+                rules={[{ required: true, message: "必须输入贷款利息！" }]}
+                noStyle
+              >
+                <InputNumber min={1} initialValues={5} defaultValue={5} />
+              </Form.Item>
+              <span className="ant-form-text"> %</span>
+            </Form.Item>
+
+            <Form.Item label="贷款年限">
+              <Form.Item
+                name="totalYear"
+                rules={[{ required: true, message: "必须输入贷款年限！" }]}
+                noStyle
+              >
+                <InputNumber min={1} initialValues={30} defaultValue={30} />
+              </Form.Item>
+              <span className="ant-form-text"> 年</span>
+            </Form.Item>
+
+            <Form.Item label="投资年化回报">
+              <Form.Item
+                name="rate"
+                rules={[{ required: true, message: "必须输入 投资年化回报！" }]}
+                noStyle
+              >
+                <InputNumber min={0} initialValues={5} defaultValue={5} />
+              </Form.Item>
+              <span className="ant-form-text"> %</span>
+            </Form.Item>
+
+            <Form.Item>
+              <Button type="primary" block htmlType="submit" size="large">
+                提交
+              </Button>
+            </Form.Item>
+          </Form>
+          <div className="code_wrapper">
+            <code id="code"> </code>
+          </div>
+        </header>
+        {data ? <Area {...config} /> : null}
+      </div>
+    </ConfigProvider>
   );
 }
 
---
+++ src/utils.js
@@ -0,0 +1,92 @@
+function getDebx(totalM, rate, totalY) {
+  let monthRate = rate / 100 / 12;
+  let totalMonth = totalY * 12;
+  return (
+    (totalM * (monthRate * (1 + monthRate) ** totalMonth)) /
+    ((1 + monthRate) ** totalMonth - 1)
+  ).toFixed(2);
+}
+// 获取等额本金第一个月还款
+function getFirstMonthDebj(totalM, rate, totalY) {
+  let monthRate = rate / 100 / 12;
+  let totalMonth = totalY * 12;
+  return (totalM / totalMonth + totalM * monthRate).toFixed(2);
+}
+
+// 获取等额本金每个月递减金额
+function getDebjDetla(totalM, rate, totalY) {
+  let monthRate = rate / 100 / 12;
+  let totalMonth = totalY * 12;
+  return ((totalM / totalMonth) * monthRate).toFixed(2);
+}
+
+// 利滚利
+let json = []
+function getMoneyCompound(r, t, debj, debx, delta) {
+  let total = 0;
+  for (let i = 0; i < t * 12; i++) {
+    let curDebj = debj - delta * i;
+    let realM = (curDebj - debx).toFixed(2);
+    console.log(`第${i + 1}月省下`, realM);
+    let curM = (realM * (1 + r / 12) ** (t * 12 - i - 1)).toFixed(2);
+    let earnM = (curM - realM).toFixed(2);
+    console.log(
+      `连本带利继续定投${t * 12 - i - 1}月，总共 赚 ${earnM} 利息`
+    );
+    total += earnM * 1;
+    json.push({
+      title: '等额本息',
+      date: i+1,
+      value: Math.round(debx * 1)
+    })
+    json.push({
+      title: '等额本金',
+      date: i+1,
+      value: Math.round(curDebj* 1)
+    })
+    json.push({
+      title: '当月少交银行金额',
+      date: i+1,
+      value: Math.round(realM* 1)
+    })
+    json.push({
+      title: '少交银行金额产生定投利息',
+      date: i+1,
+      value: Math.round(earnM * 1)
+    })
+  }
+  return total;
+}
+
+// function
+export default function compute(values) {
+  const totalMoney = values.totalMoney * 10000; // 贷款总额 单位 元
+  const MortgageInterest = values.mortgageInterest * 1; // 贷款利息 单位 %
+  const totalYear = values.totalYear * 1; // 借贷年数
+  const rate = values.rate * 1; // 投资利息 单位 %
+
+  const debx = getDebx(totalMoney, MortgageInterest, totalYear);
+  const debj = getFirstMonthDebj(totalMoney, MortgageInterest, totalYear);
+  const delta = getDebjDetla(totalMoney, MortgageInterest, totalYear);
+  let debxTotal = debx * totalYear * 12;
+  let debjTotal =
+    ((debj * 1 + (debj - (12 * totalYear - 1) * delta)) *
+      totalYear *
+      12) /
+    2;
+  let debxTouzhi = getMoneyCompound(rate / 100, totalYear, debj, debx, delta, json);
+  let text = `
+    基础信息：
+    贷款期限 ${totalYear} 年 
+    等额本息： 需要每月还${debx}, 还款总额为 ${debxTotal}；
+    等额本金： 需要第一月还${debj} 每月递减 ${delta}， 还款总额为 ${debjTotal.toFixed(2)}。
+    利滚利定投：
+    投资利率 ${rate}%
+    等额本息 去掉 等额本金后期投资(机会成本) 共赚取 ${debxTouzhi} 定投利息；
+    等额本金比等额本息少交利息 ${(debxTotal - debjTotal).toFixed(2)}；
+    个人总收益  ${(debxTouzhi - (debxTotal - debjTotal)).toFixed(2)}
+  `
+  document.querySelector("#code").innerHTML = text
+  console.log(text)
+  return json
+};
